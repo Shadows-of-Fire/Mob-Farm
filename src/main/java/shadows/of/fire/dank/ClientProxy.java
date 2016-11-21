d49e9631e71bb0ae6fcd010730a64332d8c0ab43
p455w0rd <p455w0rd@gmail.com>
2016-11-20 23:52:41 -0500

    resurrect Harambe

--- src/main/java/shadows/of/fire/dank/ClientProxy.java
+++ src/main/java/shadows/of/fire/dank/ClientProxy.java
@@ -1,19 +1,19 @@
 package shadows.of.fire.dank;
 
-
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class ClientProxy extends CommonProxy{
+public class ClientProxy extends CommonProxy {
 	@Override
 	public void preInit(FMLPreInitializationEvent e) {
 		super.preInit(e);
-	ModRegistry.initModels();
+		ModRegistry.initModels();
 	}
-	
 
+	@Override
+	public World getLocalWorld() {
+		return Minecraft.getMinecraft().theWorld;
+	}
 
-	
-	
-	
-	
 }
--- src/main/java/shadows/of/fire/dank/CommonProxy.java
+++ src/main/java/shadows/of/fire/dank/CommonProxy.java
@@ -1,26 +1,26 @@
 package shadows.of.fire.dank;
 
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import shadows.of.fire.dank.*;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraft.item.Item;
-import net.minecraftforge.fml.common.SidedProxy;
-
+import net.minecraft.world.World;
+import net.minecraftforge.common.DimensionManager;
+import net.minecraftforge.fml.common.event.*;
 
 public class CommonProxy {
-    public void preInit(FMLPreInitializationEvent e) {
-        // Initialization of blocks and items typically goes here:
-    	ModRegistry.init();
-    	RecipeManager.initRecipes();
-    }
+	public void preInit(FMLPreInitializationEvent e) {
+		// Initialization of blocks and items typically goes here:
+		ModRegistry.init();
+		RecipeManager.initRecipes();
+	}
 
-    public void init(FMLInitializationEvent e) {
+	public void init(FMLInitializationEvent e) {
 
-    }
+	}
 
-    public void postInit(FMLPostInitializationEvent e) {
+	public void postInit(FMLPostInitializationEvent e) {
 
-    }
+	}
+
+	public World getLocalWorld() {
+		return DimensionManager.getWorld(0);
+	}
 
 }
