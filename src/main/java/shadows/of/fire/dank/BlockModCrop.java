e66a08f4aba83eac4d2b45e10fee0addc12b8935
p455w0rd <p455w0rd@gmail.com>
2016-11-20 22:42:30 -0500

    add skull drop option

--- src/main/java/shadows/of/fire/dank/BlockModCrop.java
+++ src/main/java/shadows/of/fire/dank/BlockModCrop.java
@@ -1,25 +1,25 @@
 package shadows.of.fire.dank;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
+import net.minecraft.init.Items;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.*;
 
-public class BlockModCrop extends BlockCrops{
+public class BlockModCrop extends BlockCrops {
 	public Item seed;
 	public Item crop;
 	public String regname;
+	private String skullName;
+
 	public BlockModCrop(String name, Item seedIn, Item cropIn) {
 		regname = name;
 		crop = cropIn;
@@ -27,36 +27,58 @@ public class BlockModCrop extends BlockCrops{
 		setRegistryName(regname);
 		GameRegistry.register(this);
 		GameRegistry.register(new ItemBlock(this), getRegistryName());
-		
+
 	}
+
+	public BlockModCrop(String name, Item seedIn, Item cropIn, String skullName) {
+		regname = name;
+		crop = cropIn;
+		setUnlocalizedName(CancerPlants.MODID + "." + name);
+		setRegistryName(regname);
+		GameRegistry.register(this);
+		GameRegistry.register(new ItemBlock(this), getRegistryName());
+		this.skullName = skullName;
+	}
+
 	@Override
 	protected Item getSeed() {
 		return Item.getByNameOrId("cancerplants:seed" + regname.substring(4));
 	}
+
 	@Override
 	protected Item getCrop() {
 		return crop;
 	}
+
 	@Override
-	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state){
-	return true;
+	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+		return true;
 	}
+
 	@SideOnly(Side.CLIENT)
-	public void initModel(){
+	public void initModel() {
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(regname, "inventory"));
 	}
-    @Override
-    public java.util.List<ItemStack> getDrops(net.minecraft.world.IBlockAccess world, BlockPos pos, IBlockState state, int fortune)
-    {
-        java.util.List<ItemStack> ret = new ArrayList<ItemStack>();
-        ret.add(new ItemStack(this.getSeed()));
-        int age = getAge(state);
 
-        if (age >= getMaxAge())
-        {
-        ret.add(new ItemStack(this.getCrop()));
-        }
-        
-        return ret;
-    }
+	@Override
+	public java.util.List<ItemStack> getDrops(net.minecraft.world.IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+		java.util.List<ItemStack> ret = new ArrayList<ItemStack>();
+		ret.add(new ItemStack(getSeed()));
+		int age = getAge(state);
+
+		if (age >= getMaxAge()) {
+			if (getCrop() instanceof ItemSkull) {
+				ItemStack itemstack = new ItemStack(Items.SKULL, 1, 3);
+				NBTTagCompound nbt = new NBTTagCompound();
+				nbt.setTag("SkullOwner", new NBTTagString(skullName != "" ? skullName : "Notch"));
+				itemstack.setTagCompound(nbt);
+				ret.add(itemstack);
+			}
+			else {
+				ret.add(new ItemStack(getCrop()));
+			}
+		}
+
+		return ret;
+	}
 }
--- src/main/java/shadows/of/fire/dank/ModRegistry.java
+++ src/main/java/shadows/of/fire/dank/ModRegistry.java
@@ -1,44 +1,33 @@
 package shadows.of.fire.dank;
 
 import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.*;
 
 public class ModRegistry {
 
-	
 	//public ItemModSeed(String name, Block crops)
-	
+
 	public static BlockModCrop cropBone;
 	public static ItemModSeed seedBone;
 	public static BlockModCrop cropGold;
 	public static ItemModSeed seedGold;
 	public static BlockModCrop cropP455;
 	public static ItemModSeed seedP455;
-	
-	ItemStack itemstack = new ItemStack(Items.SKULL, 1, 3);
-    NBTTagCompound nbt = new NBTTagCompound();
-    nbt.setTag("SkullOwner", new NBTTagString("TheRealp455w0rd"));
-    itemstack.setTagCompound(nbt);
-	
+
 	public static void init() {
-		cropBone = new BlockModCrop("cropBone", ModRegistry.seedBone, Items.BONE); 
+		cropBone = new BlockModCrop("cropBone", ModRegistry.seedBone, Items.BONE);
 		seedBone = new ItemModSeed("seedBone", ModRegistry.cropBone);
 		cropGold = new BlockModCrop("cropGold", ModRegistry.seedGold, Items.GOLD_NUGGET);
 		seedGold = new ItemModSeed("seedGold", ModRegistry.cropGold);
-		cropP455 = new BlockModCrop("cropP455", ModRegistry.seedGold, Items.SKULL);
-		seedP455 = new ItemModSeed("seedP455", ModRegistry.cropGold);
+		cropP455 = new BlockModCrop("cropP455", ModRegistry.seedP455, Items.SKULL, "TheRealp455w0rd");
+		seedP455 = new ItemModSeed("seedP455", ModRegistry.cropP455);
 	}
-		
-	
+
 	@SideOnly(Side.CLIENT)
-	public static void initModels(){//this is only needed for seeds, but why?
+	public static void initModels() {//this is only needed for seeds, but why?
 		seedBone.initModel();
 		seedGold.initModel();
 		seedP455.initModel();
 	}
-	
-	
+
 }
