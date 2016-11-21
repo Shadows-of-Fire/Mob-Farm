3e04f491297f6a34b05d40f7be4e7594325bf34f
Shadows_of_Fire <Bward7864@gmail.com>
2016-11-21 13:29:35 -0500

    remove testing plants
    preparing for update to MobFarm.

--- src/main/java/shadows/of/fire/dank/ModRegistry.java
+++ src/main/java/shadows/of/fire/dank/ModRegistry.java
@@ -7,20 +7,20 @@ public class ModRegistry {
 
 	//public ItemModSeed(String name, Block crops)
 
-	public static BlockModCrop cropBone;
-	public static ItemModSeed seedBone;
-	public static BlockModCrop cropGold;
-	public static ItemModSeed seedGold;
+	//public static BlockModCrop cropBone;
+	//public static ItemModSeed seedBone;
+	//public static BlockModCrop cropGold;
+	//public static ItemModSeed seedGold;
 	public static BlockModCrop cropP455;
 	public static ItemModSeed seedP455;
 	public static BlockEntityCrop cropBlaze;
 	public static ItemModSeed seedBlaze;
 
 	public static void init() {
-		cropBone = new BlockModCrop("cropBone", ModRegistry.seedBone, Items.BONE);
-		seedBone = new ItemModSeed("seedBone", ModRegistry.cropBone);
-		cropGold = new BlockModCrop("cropGold", ModRegistry.seedGold, Items.GOLD_NUGGET);
-		seedGold = new ItemModSeed("seedGold", ModRegistry.cropGold);
+	//	cropBone = new BlockModCrop("cropBone", ModRegistry.seedBone, Items.BONE);
+	//	seedBone = new ItemModSeed("seedBone", ModRegistry.cropBone);
+	//	cropGold = new BlockModCrop("cropGold", ModRegistry.seedGold, Items.GOLD_NUGGET);
+	//	seedGold = new ItemModSeed("seedGold", ModRegistry.cropGold);
 		cropP455 = new BlockModCrop("cropP455", ModRegistry.seedP455, Items.SKULL, "TheRealp455w0rd");
 		seedP455 = new ItemModSeed("seedP455", ModRegistry.cropP455);
 		cropBlaze = new BlockEntityCrop("cropBlaze", ModRegistry.seedBlaze);
@@ -29,8 +29,8 @@ public class ModRegistry {
 
 	@SideOnly(Side.CLIENT)
 	public static void initModels() {//this is only needed for seeds, but why?
-		seedBone.initModel();
-		seedGold.initModel();
+	//	seedBone.initModel();
+	//	seedGold.initModel();
 		seedP455.initModel();
 		seedBlaze.initModel();
 	}
