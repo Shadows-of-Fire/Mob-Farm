9485a5a497a8fb8934f0f5a91e4b5fac4c3efa28
Shadows_of_Fire <Bward7864@gmail.com>
2017-01-20 10:25:44 -0500

    Fix dedi, Closes #1

--- src/main/java/shadows/mobfarm/MobFarm.java
+++ src/main/java/shadows/mobfarm/MobFarm.java
@@ -13,7 +13,7 @@ public class MobFarm
     public static final String MODNAME = "Mob Farm";
     public static final String VERSION = "1.0.0";
     
-    @SidedProxy(clientSide = "shadows.mobfarm.ClientProxy", serverSide = "shadows.mobfarm.ServerProxy")
+    @SidedProxy(clientSide = "shadows.mobfarm.ClientProxy", serverSide = "shadows.mobfarm.CommonProxy")
     public static CommonProxy proxy;
     
     @Mod.Instance
