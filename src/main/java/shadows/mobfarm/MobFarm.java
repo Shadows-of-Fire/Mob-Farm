d9c7c9d8c8d2c1572fbf6bfacc96f22f4fdebbcc
Shadows_of_Fire <Bward7864@gmail.com>
2017-01-31 18:27:54 -0500

    update version

--- src/main/java/shadows/mobfarm/MobFarm.java
+++ src/main/java/shadows/mobfarm/MobFarm.java
@@ -11,7 +11,7 @@ public class MobFarm
 {
     public static final String MODID = "mobfarm";
     public static final String MODNAME = "Mob Farm";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.2.0";
     
     @SidedProxy(clientSide = "shadows.mobfarm.ClientProxy", serverSide = "shadows.mobfarm.CommonProxy")
     public static CommonProxy proxy;
